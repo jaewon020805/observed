56450035c1966b286d749e7839d6877d0fd394d3
jaewon <jaewon020805@gmail.com>
2026-04-22 22:07:15 +0900

    [chore] custom_calendar_design

--- observed/lib/presentation/components/custom_calendar.dart
+++ observed/lib/presentation/components/custom_calendar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
 import 'package:observed/core/theme/app_theme.dart';
 import 'package:observed/core/theme/app_text_styles.dart';
@@ -12,111 +13,144 @@ class CustomCalendar extends StatefulWidget {
 }
 
 class _CustomCalendarState extends State<CustomCalendar> {
-  DateTime _selectedDate = DateTime.now();
-  late DateTime _focusedDate;
+  CalendarFormat _calendarFormat = CalendarFormat.week;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   void initState() {
     super.initState();
-    _focusedDate = _selectedDate;
-  }
-
-  // Get days of the week starting from a specific date (e.g., current week)
-  List<DateTime> _getWeekDays(DateTime date) {
-    // Find the Monday of the current week
-    int dayOffset = date.weekday - 1;
-    DateTime monday = date.subtract(Duration(days: dayOffset));
-    return List.generate(7, (index) => monday.add(Duration(days: index)));
+    _selectedDay = _focusedDay;
   }
 
   @override
   Widget build(BuildContext context) {
     final customColors = Theme.of(context).custom;
-    final weekDays = _getWeekDays(_focusedDate);
 
     return Column(
       children: [
-        // Header: < Month, Day(weekday) >
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              onPressed: () {
-                setState(() {
-                  _focusedDate = _focusedDate.subtract(const Duration(days: 7));
-                });
-              },
-              icon: Icon(
+        // Using AnimatedSize to fit the content height perfectly
+        AnimatedSize(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          child: TableCalendar(
+            firstDay: DateTime.utc(2020, 1, 1),
+            lastDay: DateTime.utc(2030, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            rowHeight: 45.h, // Compact row height
+            daysOfWeekHeight: 30.h, // Compact day of week height
+            availableCalendarFormats: const {
+              CalendarFormat.month: 'Month',
+              CalendarFormat.week: 'Week',
+            },
+            locale: 'en_US',
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            onPageChanged: (focusedDay) {
+              setState(() {
+                _focusedDay = focusedDay;
+              });
+            },
+
+            // Header Style
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              titleTextStyle: AppTextStyles.m4.copyWith(
+                color: customColors.text1,
+              ),
+              leftChevronIcon: Icon(
                 Icons.chevron_left,
                 color: customColors.text1,
                 size: 24.sp,
               ),
-            ),
-            Text(
-              DateFormat('MMM, d(E)').format(_focusedDate),
-              style: AppTextStyles.m4.copyWith(color: customColors.text1),
-            ),
-            IconButton(
-              onPressed: () {
-                setState(() {
-                  _focusedDate = _focusedDate.add(const Duration(days: 7));
-                });
-              },
-              icon: Icon(
+              rightChevronIcon: Icon(
                 Icons.chevron_right,
                 color: customColors.text1,
                 size: 24.sp,
               ),
+              headerPadding: EdgeInsets.only(bottom: 10.h),
+              titleTextFormatter: (date, locale) =>
+                  DateFormat('MMM, d(E)').format(date),
             ),
-          ],
-        ),
-        SizedBox(height: 10.h),
-        // Day Labels & Dates
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: weekDays.map((date) {
-            bool isSelected = DateUtils.isSameDay(date, _selectedDate);
-            String dayName = DateFormat('E').format(date).toLowerCase();
 
-            return GestureDetector(
-              onTap: () {
-                setState(() {
-                  _selectedDate = date;
-                  _focusedDate = date;
-                });
-              },
-              child: Column(
-                children: [
-                  Text(
-                    dayName,
-                    style: AppTextStyles.m4.copyWith(
-                      color: isSelected
-                          ? customColors.text1
-                          : customColors.text2,
-                    ),
-                  ),
-                  SizedBox(height: 8.h),
-                  Text(
-                    date.day.toString(),
-                    style: AppTextStyles.m2.copyWith(
-                      color: isSelected
-                          ? customColors.text1
-                          : customColors.text2,
-                    ),
-                  ),
-                ],
+            // Days of week style
+            daysOfWeekStyle: DaysOfWeekStyle(
+              weekdayStyle: AppTextStyles.m4.copyWith(
+                color: customColors.text2,
               ),
-            );
-          }).toList(),
+              weekendStyle: AppTextStyles.m4.copyWith(
+                color: customColors.text2,
+              ),
+              dowTextFormatter: (date, locale) =>
+                  DateFormat('E').format(date).toLowerCase(),
+            ),
+
+            // Calendar Builders
+            calendarBuilders: CalendarBuilders(
+              defaultBuilder: (context, day, focusedDay) {
+                return _buildDayCell(day, customColors.text2!, false);
+              },
+              selectedBuilder: (context, day, focusedDay) {
+                return _buildDayCell(day, customColors.text1!, true);
+              },
+              todayBuilder: (context, day, focusedDay) {
+                return _buildDayCell(day, customColors.text2!, false);
+              },
+              outsideBuilder: (context, day, focusedDay) {
+                return _buildDayCell(
+                  day,
+                  customColors.text2!.withOpacity(0.3),
+                  false,
+                );
+              },
+            ),
+          ),
         ),
 
-        SizedBox(height: 20.h),
-        Container(
-          width: 24.w,
-          height: 2.h,
-          decoration: BoxDecoration(
-            color: customColors.text2,
-            borderRadius: BorderRadius.circular(2.r),
+        // Handle
+        GestureDetector(
+          onVerticalDragEnd: (details) {
+            final velocity = details.primaryVelocity ?? 0;
+            if (velocity > 0) {
+              setState(() => _calendarFormat = CalendarFormat.month);
+            } else if (velocity < 0) {
+              setState(() => _calendarFormat = CalendarFormat.week);
+            }
+          },
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 15.h), // Consistent padding
+            color: Colors.transparent,
+            width: double.infinity,
+            alignment: Alignment.center,
+            child: Container(
+              width: 24.w,
+              height: 2.h,
+              decoration: BoxDecoration(
+                color: customColors.text2,
+                borderRadius: BorderRadius.circular(2.r),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDayCell(DateTime day, Color textColor, bool isSelected) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          day.day.toString(),
+          style: AppTextStyles.m4.copyWith(
+            color: isSelected ? Theme.of(context).custom.text1 : textColor,
           ),
         ),
       ],
